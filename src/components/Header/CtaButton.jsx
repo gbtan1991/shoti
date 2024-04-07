2cc03ccc32ac79354838345380495b5620f593ad
Gilbert Tan <gilbert.tan1991@gmail.com>
2024-04-08 00:34:30 +0800

    Add the CTA Button with the pastel styling

---
+++ src/components/Header/CtaButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CtaButton = () => {
+  return (
+    <div>
+        <a href='#' className='bg-light-red text-white lg:text-2xl font-granstander p-3 lg:p-4 rounded-lg border-dashed border-white border-2'>Get Started</a>{/* Button */}
+    </div>
+  )
+}
+
+export default CtaButton
